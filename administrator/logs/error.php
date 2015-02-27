a9b5f0eed646c78150a7e94299b2a08348ea08ae
w2 <nesskraven@gmail.com>
2015-02-26 16:05:04 -0800

    Adicionando efeito Hover Grow nas imagens do trem e do mapa

---
+++ administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-02-26 02:17:36 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2015-02-26T02:17:36+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-02-26T23:37:28+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
